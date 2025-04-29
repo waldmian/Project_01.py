4e0506e78c2fa87e11cad21c5a679d0b2058caa1
waldmian <waldmian2@gmail.com>
2025-04-30 00:01:27 +0200

    Changed text for error input.

---
+++ main.py
@@ -0,0 +1,139 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Anna Waldhansová
+email: waldmian2@gmail.com
+"""
+
+TEXTS = [
+    '''Situated about 10 miles west of Kemmerer,
+    Fossil Butte is a ruggedly impressive
+    topographic feature that rises sharply
+    some 1000 feet above Twin Creek Valley
+    to an elevation of more than 7500 feet
+    above sea level. The butte is located just
+    north of US 30 and the Union Pacific Railroad,
+    which traverse the valley.''',
+    '''At the base of Fossil Butte are the bright
+    red, purple, yellow and gray beds of the Wasatch
+    Formation. Eroded portions of these horizontal
+    beds slope gradually upward from the valley floor
+    and steepen abruptly. Overlying them and extending
+    to the top of the butte are the much steeper
+    buff-to-white beds of the Green River Formation,
+    which are about 300 feet thick.''',
+    '''The monument contains 8198 acres and protects
+    a portion of the largest deposit of freshwater fish
+    fossils in the world. The richest fossil fish deposits
+    are found in multiple limestone layers, which lie some
+    100 feet below the top of the butte. The fossils
+    represent several varieties of perch, as well as
+    other freshwater genera and herring similar to those
+    in modern oceans. Other fish such as paddlefish,
+    garpike and stingray are also present.'''
+]
+
+
+
+data = """
++------+-------------+
+| user |   password  |
++------+-------------+
+| bob  |     123     |
+| ann  |   pass123   |
+| mike | password123 |
+| liz  |   pass123   |
++------+-------------+
+"""
+separator = "-"*40
+name = input("username: ")
+password = input("password: ")
+print(separator)
+greeting = (f"Hi, welcome to the app, {name.title()}. \nWe have 3 texts to be analysed.")
+
+if name == "bob" and password == "123":
+  print(greeting)
+elif name == "ann" and password == "pass123":
+  print(greeting)
+elif name == "mike" and password == "password123":
+  print(greeting)
+elif name == "liz" and password == "pass123":
+  print(greeting)
+else:
+  print("Unregistered user. Terminating the program.")
+  import sys
+  sys.exit(0)
+
+print(separator)
+
+text_nr = int(input("Enter a number btw. 1 and 3 to select: "))
+print(separator)
+text_numbers = [1, 2, 3]
+if not text_nr in text_numbers:
+  print("Wrong number selected. Terminating the program. ")
+  import sys
+  sys.exit(0)
+else:
+  paragraphs = []
+  for text in TEXTS:
+    paragraphs.append(text)
+
+    word_count = []
+    word_count_title = []
+    word_count_upper = []
+    word_count_lower = []
+    word_count_numeric = {}
+    sum_digits = []
+
+
+  for word in paragraphs[text_nr - 1].split():
+    word.strip(",.")
+    word_count.append(word)
+    if word.istitle():
+      word_count_title.append(word.title())
+
+    elif word.isupper():
+      word_count_upper.append(word.upper())
+
+    elif word.islower():
+      word_count_lower.append(word.lower())
+
+    elif word.isdigit():
+      word_count_numeric[word] = word.count(word)
+      sum_digits.append(int(word))
+
+
+print(f"There are {len(word_count)} words in the selected text.")
+print(f"There are {len(word_count_title)} titlecase words.")
+print(f"There are {len(word_count_upper)} uppercase words.")
+print(f"There are {len(word_count_lower)} lowercase words.")
+print(f"There are {len(word_count_numeric)} numeric strings.")
+print(f"The sum of all the numbers is {sum(sum_digits)}.")
+
+print(separator)
+print("LEN|  OCCURENCES  |NR.")
+print(separator)
+
+text_copy = paragraphs[text_nr - 1]
+
+separate_words = []
+separate_words_length = []
+length_occurence = {}
+
+for word in text_copy.split():
+  clean_word = word.strip(",.;")
+  separate_words.append(clean_word)
+  word_length = []
+for word in separate_words:
+  word_length.append(len(word))
+for length in word_length:
+  if length not in length_occurence:
+    length_occurence[length] = 1
+  else:
+    length_occurence[length] += 1
+
+for k, v in sorted(length_occurence.items()):
+  stars = "*"*v
+  first_width = 3
+  second_width = 20
+  print(f"{k:{first_width}}|{stars:<{second_width}}|{v}")
